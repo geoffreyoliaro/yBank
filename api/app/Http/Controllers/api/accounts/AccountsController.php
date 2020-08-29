e19b93929dfbc9cafd001063de2d75fde19c64ef
szeth <geoffreyoliaro@gmail.com>
2020-08-29 11:30:54 +0300

    create accounts api with validations

--- api/app/Http/Controllers/api/accounts/AccountsController.php
+++ api/app/Http/Controllers/api/accounts/AccountsController.php
@@ -9,17 +9,28 @@ use Illuminate\Support\Facades\DB;
 class AccountsController extends Controller
 {
     public function create(Request $request){
-         
-          $account = DB::table('accounts')->insert([
-              'name' => $request->name,
-              'balance' => $request->balance
-          ]);
-  
-        
-          return response([
-              'status'=>1,
-              'payload'=>$account,
-              'message'=>'Account created Successfuly'
-          ]);
-      }
+        $valid = validator($request->only('name', 'balance'),[
+            'name'=>'required|string',
+            'balance'=>'required'
+        ]); 
+        if($valid->fails()){
+            return response()->json(["errors"=> $valid->errors()->all()],400);
+        }
+        $account = DB::table('accounts')
+                    ->where('name',$request->name)
+                    ->exists();
+
+        if($account === true){
+            return response()->json(["message"=>"account already exists please login"],400);
+        }
+        $account = DB::table('accounts')->insert([
+            'name' => $request->name,
+            'balance' => $request->balance
+        ]);
+        return response([
+            'status'=>1,
+            'payload'=>$account,
+            'message'=>'Account created Successfuly'
+        ]);
+    }
 }
